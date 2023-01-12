d3f7850975fe95510b54aea55578a4dc66d05765
Vlad Chepaykyn <backdround@yandex.ru>
2023-01-12 18:57:09 +0300

    refactor: simplify build script

--- build-image.sh
+++ build-image.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd "$(dirname "$0")"
+#error() {
+  #echo "$@" >&2
+  #exit 1
+#}
 
-# Build script that configure archlive rootfs
-rm -rf ./output
-src/make-rootfs-configure-script.sh ./archlive-installation-script ./output archlive-rootfs-configure.sh
+cd "$(dirname "$0")"
+rm -rf ./output && mkdir ./output
+
+# Prepare installation script.
+cp -a ./archlive-installation-script ./output/installation-script
+rm -rf ./output/installation-script/.git
+cp -f ./src/installation-config ./output/installation-script/config
 
 # Bulid archlive image
-export rootfs_configure_script="output/archlive-rootfs-configure.sh"
+export rootfs_configure_script="src/rootfs-configure.sh"
 export kernel_options="rw console=ttyS0"
+export directory_to_copy="./output/installation-script"
 ./archlive-builder/run build-live-image
 
 mv ./archlive-builder/output/live.img ./output
